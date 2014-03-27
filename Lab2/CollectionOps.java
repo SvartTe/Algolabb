a0bb47379f11e1cedbfd68b642a25769bf82e656
Seabstian <seblind@student.chalmers.se>
2014-03-27 14:44:47 +0100

    The most optimized mtehod. In the world.

--- Lab2/CollectionOps.java
+++ Lab2/CollectionOps.java
@@ -6,9 +6,17 @@ import java.util.LinkedList;
 public class CollectionOps {
 
     // Put your code for print here ... 
-    
+	
     // Put your code for reverse here ...
-
+	public static <T> List<T> reverse(List<T> l){
+		for (int i = 0; i < l.size(); i++){
+			l.add(0, l.get(l.size()-1));	// Index starts from 0, size from 1, so this takes the last element
+			l.remove(l.size()-1);		// The last element is removed, l.size() is back to its starting value
+		}
+		return l;
+	}
+	
+	
     // Put your code for less here ...
     
     // Example
