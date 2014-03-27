359d69550c10191112e4f42a54f71d8ed8d7e9d1
Robert Larsson <madcat777@gmail.com>
2014-03-27 16:07:11 +0100

    Fixed indentation of given code because ADD

--- Lab2/CollectionOps.java
+++ Lab2/CollectionOps.java
@@ -34,20 +34,20 @@ public class CollectionOps {
     public static <T1,T2> Collection<T2>
     map(UnaryOp<T1,T2> functor,Collection<T1> c) 
     {
-        // Determine the dynamic type of the collection
-        Class<? extends Collection> cls = c.getClass();
-        try {
-            // Create an object of the same dynamic type as c
-            Collection<T2> result = cls.newInstance();
-            // Copy the elements and apply op to them
-            for ( T1 x : c )
-                result.add(functor.op(x));
-            return result;   
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+    	// Determine the dynamic type of the collection
+    	Class<? extends Collection> cls = c.getClass();
+    	try {
+    		// Create an object of the same dynamic type as c
+    		Collection<T2> result = cls.newInstance();
+    		// Copy the elements and apply op to them
+    		for ( T1 x : c )
+    			result.add(functor.op(x));
+    		return result;   
+    	}
+    	catch (Exception e) {
+    		e.printStackTrace();
+    		return null;
+    	}
     }
     
     // Put your code for filter here ...
